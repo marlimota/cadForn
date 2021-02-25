3c6ed9b2362fb698bb2d15e72e0e269714eaaf61
MarliMota <marli.mota42@gmail.com>
2021-02-25 11:21:52 -0300

    edit e cancel funcionando

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 var app = new function () {
   //A tabela onde serão mostrados os elementos(fornecedores)
   this.FornecedoresTable = document.getElementById('fornecedoresBody');
- 
+
   function Fornecedor(nomeFantasia, razaoSocial, cnpj, telefone, celular, endereco, email, site, produto, contrato, observacao = "") {
     this.nomeFantasia = nomeFantasia;
     this.razaoSocial = razaoSocial;
@@ -35,7 +35,9 @@ var app = new function () {
         data += '<td style="width:9%">' + this.fornecedoresList[i].razaoSocial + '</td>';
         data += '<td style="width:9%">' + this.fornecedoresList[i].cnpj + '</td>';
         data += '<td style="width:9%">' + this.fornecedoresList[i].telefone + '</td>';
-        data += '<td style="width:9%"> <Button onclick="app.ShowDetails(' + i + ')"  class="btn" id="details-btn"><i class="fa fa-eye"></i> Detalhes</Button> </td>';
+        data += '<td style="width:9%"> <Button onclick="app.Details(' + i + ')"  class="btn" id="details-btn"><i class="fa fa-ellipsis-h"></i> Detalhes</Button> </td>';
+        data += '<td style="width:9%"> <Button onclick="app.Edit(' + i + ')"  class="btn" id="edit-btn"><i class="fa fa-edit"></i> Editar</Button> </td>';
+        data += '<td style="width:9%"> <button onclick="app.Delete(' + i + ')" class="btn-delete"><i class="fa fa-times"></i></button> </td>';
         data += '</tr>';
       }
     }
@@ -82,7 +84,7 @@ var app = new function () {
     }
   };
 
-  //EDIT FUNCTION
+  //Funçao de cadastrar
   this.ShowDetails = function (item) {
 
     //Cria um objeto fornecedor que é cópia do fornecedor que se deseja editar
@@ -104,36 +106,46 @@ var app = new function () {
     //Exibe os campos de edição
     ShowNewProviderOverlay();
     self = this;
+  };
+  //funçao salvar
+  this.SaveEditedFornecedor = function (item) {
 
-    //Função que salva as edições realizadas caso sejam validas
-    document.getElementById('save-edit').onsubmit = function () {
+    //Cria um objeto fornecedor com todas as informações contidas nos campos de edição
+    let fornecedor = new Fornecedor(
+      document.getElementById('nomeFantasia').value,
+      document.getElementById('razaoSocial').value,
+      document.getElementById('cnpj').value,
+      document.getElementById('telefone').value,
+      document.getElementById('celular').value,
+      document.getElementById('endereco').value,
+      document.getElementById('email').value,
+      document.getElementById('site').value,
+      document.getElementById('produto').value,
+      document.getElementById('contrato').value,
+      document.getElementById('observacao').value
+    );
 
-      //Cria um objeto fornecedor com todas as informações contidas nos campos de edição
-      let editedFornecedor = new Fornecedor(
-        document.getElementById('nomeFantasia').value,
-        document.getElementById('razaoSocial').value,
-        document.getElementById('cnpj').value,
-        document.getElementById('telefone').value,
-        document.getElementById('celular').value,
-        document.getElementById('endereco').value,
-        document.getElementById('email').value,
-        document.getElementById('site').value,
-        document.getElementById('produto').value,
-        document.getElementById('contrato').value,
-        document.getElementById('observacao').value
-      );
-
-      //Caso todas as informações sejam válidas atualiza a lista de fornecedores e esconde os campos de edição
-      if (isFornecedorValid(editedFornecedor)) {
-        // Edit value
-        self.fornecedoresList.splice(item, 1, editedFornecedor);
-        // Display the new list
-        self.FetchAll();
-        // Hide fields
-        CloseInput();
-      }
+    //Caso todas as informações sejam válidas atualiza a lista de fornecedores e esconde os campos de edição
+    if (isFornecedorValid(fornecedor)) {
+      // Edit value
+      this.fornecedoresList.splice(item, 1, fornecedor);
+      // Display the new list
+      this.FetchAll();
+      // Hide fields
+      HideNewProviderOverlay();
     }
   };
+
+  //edit function
+
+  this.Edit = function (item) {
+    this.ShowOverlay(item);
+    this.SetReadOnly(false);
+
+    document.getElementById("newFornecedor").style.display = "none";
+    document.getElementById("edit").style.display = "flex";
+    document.getElementById("details").style.display = "none";
+  }
 
   //DELETE FUNCTION
   this.Delete = function (item) {
@@ -143,12 +155,70 @@ var app = new function () {
     this.FetchAll();
   };
 
-}
+  //DETAILS FUNCTION  
+
+  this.Details = function (item) {
+    this.ShowOverlay(item);
+    this.SetReadOnly(true);
+    document.getElementById("newFornecedor").style.display = "none";
+    document.getElementById("edit").style.display = "none";
+    document.getElementById("details").style.display = "flex";
+  }
+
+  this.ShowOverlay = function (item) {
+    document.getElementById("pageOverlay").style.display = "block";
+    document.getElementById("nomeFantasia").value = this.fornecedoresList[item].nomeFantasia;
+    document.getElementById("razaoSocial").value = this.fornecedoresList[item].razaoSocial;
+    document.getElementById("cnpj").value = this.fornecedoresList[item].cnpj;
+    document.getElementById("telefone").value = this.fornecedoresList[item].telefone;
+    document.getElementById("celular").value = this.fornecedoresList[item].celular;
+    document.getElementById("endereco").value = this.fornecedoresList[item].endereco;
+    document.getElementById("email").value = this.fornecedoresList[item].email;
+    document.getElementById("site").value = this.fornecedoresList[item].site;
+    document.getElementById("produto").value = this.fornecedoresList[item].produto;
+    document.getElementById("contrato").value = this.fornecedoresList[item].contrato;
+    document.getElementById("observacao").value = this.fornecedoresList[item].observacao;
+  }
+
+  this.SetReadOnly = function (value) {
+    document.getElementById("nomeFantasia").readOnly = value;
+    document.getElementById("razaoSocial").readOnly = value;
+    document.getElementById("cnpj").readOnly = value;
+    document.getElementById("telefone").readOnly = value;
+    document.getElementById("celular").readOnly = value;
+    document.getElementById("endereco").readOnly = value;
+    document.getElementById("email").readOnly = value;
+    document.getElementById("site").readOnly = value;
+    document.getElementById("produto").readOnly = value;
+    document.getElementById("contrato").readOnly = value;
+    document.getElementById("observacao").readOnly = value;
+  }
+
+  let initialProvider = new Fornecedor(
+    "Marli",
+    "Mota",
+    "00.000.000/0000-00",
+    "(00) 0000-0000",
+    "(00) 00000-0000",
+    "Rua das Ruas, 99 - São Paulo SP",
+    "marli@mota.com",
+    "www.marli.com.br",
+    "Softwares",
+    "000000000000001",
+    ""
+  );
+
+  this.fornecedoresList.push(initialProvider);
+};
+
 
 app.FetchAll();
 
 this.ShowNewProviderOverlay = function () {
   document.getElementById("pageOverlay").style.display = "block";
+  document.getElementById("edit").style.display = "none";
+  document.getElementById("details").style.display = "none";
+  document.getElementById("newFornecedor").style.display = "flex";
 }
 
 this.HideNewProviderOverlay = function () {
